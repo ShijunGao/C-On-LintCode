8ddbd7ebc6328ee23a8ec620aecda01b9f2f457b
Gary <SJGDUT@gmail.com>
2017-09-13 23:19:08 +0800

    add annotation of this code

--- readBmp.cpp
+++ readBmp.cpp
@@ -1,4 +1,4 @@
-#include <string.h>     
+#include <string.h>		//memset()
 #include <math.h>       
 #include <stdio.h>       
 #include <stdlib.h>       
@@ -9,52 +9,56 @@
 
 #pragma warning(disable:4996)
 
-typedef unsigned char  BYTE;
-typedef unsigned short WORD;
-typedef unsigned long  DWORD;
-typedef long LONG;
+//typedef 定义新的类型
+
+
+typedef unsigned char  BYTE;	//无符号字节型，one Byte	
+typedef unsigned short WORD;	//无符号短整型，two Bytes
+typedef unsigned long  DWORD;	//无符号长整型，eight Bytes
+typedef long LONG;				//长整型，eight Bytes
 
 //头文件
 
-typedef struct tagBITMAPFILEHEADER {			
-	//共14Byte 2+4+2+2+4
-
-	WORD bType;			//文件类型，必须是0x42 (B,低字节), 0x4D (M,高字节) 0x4D42
-	DWORD bfSize;		//文件大小
-	WORD bfReserved1;	//保留字，不考虑？
-	WORD bfReserved2;	//保留字，+1；
-	DWORD bfOffBits;	//偏移量，本次使用的是54
-}BITMAPFILEHEADER;
-
-//位图信息头
-
-typedef struct tagBITMAPINFOHEADER {
-	//40Byte  4+4+4+2+2+4+4+4+4+4+4
-
-	DWORD biSize;			//结构体tagBITMAPINFOHEADER所占字节数
-	LONG biWidth;			//图像宽度 pixel
-	LONG biHeight;			//高度 pixel
-	WORD biPlanes;			//目标设备级别？===1
-	WORD biBitCount;		//本次为24位真彩图
-							//每个像素所需位数 
-							// 1位 --黑白
-							// 4、8、256
-	DWORD biCompression;	//压缩类型 
-							// 0 --不压缩
-							// 1 --BI_RLE8
-							// 2 --BI_RLE4
-	DWORD biSizeImage;		//位图数据大小 biWidth * biHeight (biWidth补齐4的倍数)
-	LONG biXPelsPerMeter;	//水平分辨率
-	LONG biYPelsPermeter;	//垂直分辨率
-	DWORD biClrUsed;		//实际使用颜色数
-	DWORD biClrImportant;	//重要颜色数 0--all
-}BITMAPINFOHEADER;
+//以下结构体C++中已定义
+//typedef struct tagBITMAPFILEHEADER {			
+//	//共14Byte 2+4+2+2+4
+//
+//	WORD bType;			//文件类型，必须是0x42 (B,低字节), 0x4D (M,高字节) 0x4D42
+//	DWORD bfSize;		//文件大小
+//	WORD bfReserved1;	//保留字，不考虑？
+//	WORD bfReserved2;	//保留字，+1；
+//	DWORD bfOffBits;	//偏移量，本次使用的是54
+//}BITMAPFILEHEADER;
+//
+////位图信息头
+//
+//typedef struct tagBITMAPINFOHEADER {
+//	//40Byte  4+4+4+2+2+4+4+4+4+4+4
+//
+//	DWORD biSize;			//结构体tagBITMAPINFOHEADER所占字节数
+//	LONG biWidth;			//图像宽度 pixel
+//	LONG biHeight;			//高度 pixel
+//	WORD biPlanes;			//目标设备级别？===1
+//	WORD biBitCount;		//本次为24位真彩图
+//							//每个像素所需位数 
+//							// 1位 --黑白
+//							// 4、8、256
+//	DWORD biCompression;	//压缩类型 
+//							// 0 --不压缩
+//							// 1 --BI_RLE8
+//							// 2 --BI_RLE4
+//	DWORD biSizeImage;		//位图数据大小 biWidth * biHeight (biWidth补齐4的倍数)
+//	LONG biXPelsPerMeter;	//水平分辨率
+//	LONG biYPelsPermeter;	//垂直分辨率
+//	DWORD biClrUsed;		//实际使用颜色数
+//	DWORD biClrImportant;	//重要颜色数 0--all
+//}BITMAPINFOHEADER;
 void main(){
 	BITMAPFILEHEADER bitHead;
 	BITMAPINFOHEADER bitInfoHead;
 	FILE* pfile;
 
-	char strFile[50] = "G:\\images\\test.bmp";	//打开图像路径，需修改为自己图像存储的路径  
+	char strFile[50] = "..\\test.bmp";	//打开图像路径，需修改为自己图像存储的路径  
 	pfile = fopen(strFile, "rb");				//文件打开图像  
 
 									  
@@ -65,7 +69,7 @@ void main(){
 		printf("file is not .bmp file!");
 		return;
 	}
-	fread(&bitHead, 1, sizeof(tagBITMAPFILEHEADER), pfile);		//读取文件头
+	fread(&bitHead, 1, sizeof(BITMAPFILEHEADER), pfile);		//读取文件头
 	fread(&bitInfoHead, 1, sizeof(BITMAPINFOHEADER), pfile);	//读取位图信息头信息
 
 	int width = bitInfoHead.biWidth;							//行宽 pixel
